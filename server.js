d64a7fe11f6ad2451ef9ff0c0311a69149584a92
Semen Kologrivov <firedragon855@gmail.com>
2019-06-24 19:35:04 +0700

    Custom plugin for caching

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+    chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+    pageDoneCheckInterval: 100 
+});
 
 server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.blockResources());
+server.use(require('./lib/plugins/redisCache.js'));
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
